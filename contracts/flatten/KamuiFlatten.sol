52a6ed22d69f32eb137fd722f6f43ae02de5a15b
Brian <pianochicken1996@gmail.com>
2023-06-24 13:45:18 +0800

    feat: support Sepolia & Mumbai chain

--- contracts/flatten/KamuiFlatten.sol
+++ contracts/flatten/KamuiFlatten.sol
@@ -607,7 +607,7 @@ contract ERC20 is IERC20, IERC20Metadata {
 pragma solidity ^0.8.0;
 
 
-contract Kamui is ERC20 {
+contract KamuiFlatten is ERC20 {
 
     constructor() ERC20("WrappedEther", "WETH") {
         _mint(msg.sender, 1000000 ether);
